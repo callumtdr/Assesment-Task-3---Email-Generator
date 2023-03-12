882ab7f5384e22aeeaa469cc64d4af113577d006
callumtdr <107794557+callumtdr@users.noreply.github.com>
2023-03-12 12:47:39 +0000

    commented

--- Generator.cs
+++ Generator.cs
@@ -42,17 +42,17 @@ using System.Threading.Tasks;
 
             if (names.Length != 2) //Checks wether the name array does not have 2 names. 
             {
-                Console.WriteLine("ERROR: Enter First Name, Second Name,and Use Characters only");
-                return false;
+                Console.WriteLine("ERROR: Enter First Name, Second Name,and Use Characters only"); // If there are not 2 names, prints an error message to the console.
+                return false; //returns false if there are not 2 names.
             }
 
 
             foreach (char c in names[0]) //Checks if the first name in the array only has characters.
             {
-                if (!char.IsLetter(c))
+                if (!char.IsLetter(c)) //Checks if "c" is not a letter.
                 {
-                    Console.WriteLine("ERROR: Enter First Name, Second Name,and Use Characters only");
-                    return false;
+                    Console.WriteLine("ERROR: Enter First Name, Second Name,and Use Characters only"); //If "c" is not a letter, prints an error message to the console.
+                    return false; //Returns false if the name is invalid.
                 
                 }
             
@@ -60,10 +60,10 @@ using System.Threading.Tasks;
 
             foreach (char c in names[1]) //Checks if the second name only has characters.
             {
-                if (!char.IsLetter(c))
+                if (!char.IsLetter(c)) //checks if "c" is not a letter.
                 {
-                    Console.WriteLine("ERROR: Enter First Name, Second Name,and Use Characters only");
-                    return false;
+                    Console.WriteLine("ERROR: Enter First Name, Second Name,and Use Characters only"); //If "c" is not a letter, prints an error message to the console.
+                    return false; //Returns false if the name is invalid.
                 }
             }
 
